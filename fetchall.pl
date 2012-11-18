7019a421de96a2bbab29a6c05fca9b6dd70ac553
Jyun-Fan Tsai <jyunfan@gmail.com>
2012-11-18 11:16:44 +0800

    fix error in fetchall.  add dep package mkdirp

--- fetchall.pl
+++ fetchall.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/env perl -w
 use strict;
-system('mkdir -p meta')
-for my $i (3109..4007) {
-    system("sh ./list $i > meta/$i.html");
+system('mkdir -p meta');
+#for my $i (3109..4007) {
+for my $i (3287..3990) {
+    system("sh ./list $i > source/meta/$i.html");
     sleep(1);
 }
