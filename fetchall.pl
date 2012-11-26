2a1c9553146f3a8c92c1deb39a73a42d19926e2d
Chia-liang Kao <clkao@clkao.org>
2012-11-26 21:19:20 +0800

    update index

--- fetchall.pl
+++ fetchall.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl -w
 use strict;
 system('mkdir -p meta');
-for my $i (3109..4007) {
+for my $i (3109..4008) {
     system("sh ./list $i > source/meta/$i.html");
     sleep(1);
 }
