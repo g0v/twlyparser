0a1e82df5890a331569ae3d68dd3b9ec4297dc6c
chhsiao <chhsiao@ikala.tv>
2013-01-30 23:06:13 +0800

    add extra related

---
+++ README.extra.md
@@ -0,0 +1,8 @@
+extra docs
+============
+
+### Quick start: 
+
+./gen_extra_index_all.sh
+
+(You need data/index.json for gazette no. reference.)
---
+++ gen_extra_index.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "php gen_extra_index.php data/extra/extra_files.txt > data/index.extra.json"
+php gen_extra_index.php data/extra/extra_files.txt > data/index.extra.json
