d7de97599be8b4241a8ff6f01a1620aaa56224bc
chhsiao <chhsiao@ikala.tv>
2013-02-02 00:30:43 +0800

    trying to refactor

--- index-merger.sh
+++ index-merger.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ -f data/index.json.orig.for_merge ]
+if [ ! -f data/index.json.orig.for_merge ]
   then
   cp data/index.json data/index.json.orig.for_merge
 fi
-./node_modules/.bin/lsc index-merger.ls data/index.json data/index.extra.json > data/index.merge.json
+./node_modules/.bin/lsc -d index-merger.ls data/index.json data/index.extra.json > data/index.merge.json
 mv data/index.merge.json data/index.json
