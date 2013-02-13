203e618c4a90491ccdf1cf81a6c7f7a540741e14
chhsiao <hsiao.chuanheng@gmail.com>
2013-02-13 23:56:08 +0800

    improve parse-list (branch directly from 251972b38154d30494bad237746ac07606c5139a)

---
+++ script/gen-format-committee-log.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "${BASH_ARGC}" == 1 ]
+  then
+  data_dir=${BASH_ARGV[0]}
+else
+  data_dir=output
+fi
+
+current_dir=`pwd`
+base_current_dir=`echo "${current_dir}"|sed '/s.*\//g'`
+if [ "${base_current_dir}" == "script" ]
+then
+  cd ..
+fi
+
+source script/set_range.sh
+
+for ((j = ${end}; j >= ${start}; j--))
+do
+  echo "committee: ${j}"
+  ./node_modules/.bin/lsc format-committee-log.ls --gazette ${j} --dir ${data_dir} --text
+done
+
+cd ${current_dir}
